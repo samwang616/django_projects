2585488dded12d3259d55da4434e33d174b780d3
Sam Wang <samwang616@gmail.com>
2020-01-23 16:46:46 +0000

    after skeleton

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=x44cwfrwcav!!$a2%7njy_*zc5mu%mw15tr42wewzfe40p#2g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
